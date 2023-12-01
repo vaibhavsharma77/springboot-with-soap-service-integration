52328ca53c368e3efd000b4a9cd1b10c8b3b0038
Vaibhav Sharma <vaibhavsharma777@yahoo.com>
2023-12-01 19:22:35 +0100

    adding maven dependency and spring soap web service dependency with project skeleton

---
+++ src/main/java/com/soap/example/soapserviceintegration/SoapServiceIntegrationApplication.java
@@ -0,0 +1,13 @@
+package com.soap.example.soapserviceintegration;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SoapServiceIntegrationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SoapServiceIntegrationApplication.class, args);
+	}
+
+}
